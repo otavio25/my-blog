5badac32fa5b9cdd2c0379803568fcef7a5b5d09
otavio25 <souzaotavio721@gmail.com>
2023-01-27 23:09:19 -0300

    adicionando responsividade ao header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom'
 import './styles.css'
+import {FiMenu} from 'react-icons/fi'
 
 function Header(){
+    const [menuOpen, setMenuOpen] = useState(true)
+
+    useEffect(()=>{
+        window.addEventListener('resize', ()=>{
+            if(window.innerWidth >= 999){
+                setMenuOpen(true)
+            }
+        })
+    }, [])
+
     return (
         <header>
             <nav >
                 <Link className='logo' to="/">Otávio</Link>
-                <ul className='nav-list'>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/sobre">Sobre</Link></li>
-                    <li><Link to="/curriculo">Currículo</Link></li>
-                    <li><Link to="/contato">Contato</Link></li>
-                </ul>
+                <div className='mobile-menu'>
+                    <FiMenu size={30} color='#fff' onClick={()=>{
+                        setMenuOpen(!menuOpen)
+                    }}/>
+                </div>
+                {menuOpen && (
+                    <ul className='nav-list'>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/sobre">Sobre</Link></li>
+                        <li><Link to="/curriculo">Currículo</Link></li>
+                        <li><Link to="/contato">Contato</Link></li>
+                    </ul>
+                )}
             </nav>
         </header>
     )
