d2efb4643ec46420b4ac2c27800da9649b7b9778
otavio25 <souzaotavio721@gmail.com>
2023-07-06 00:10:03 -0300

    adicionando modal e carrousel

--- src/frontend/src/components/layouts/Header.js
+++ src/frontend/src/components/layouts/Header.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import { Container, Navbar, Nav, Button, Offcanvas } from 'react-bootstrap';
+import { Container, Navbar, Nav, Button, Modal } from 'react-bootstrap';
 import Logo from '../img/logo.png'
 
 function Header(){
     const [show, setShow] = useState(false);
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
@@ -21,22 +22,32 @@ function Header(){
                                 <Button variant="dark" href="#home">Home</Button>
                             </Nav>
                             <Nav>
+                                <Button variant="dark" href="#conteudo">Conteúdo</Button>
                                 <Button variant="dark" href="#curriculo">Currículo</Button>
                                 <Button variant="dark" href="#contato">Contato</Button>
                                 <Button variant="dark" onClick={handleShow}> Sobre </Button>
                             </Nav>
-                            <Navbar.Offcanvas show={show} onHide={handleClose}>
-                                <Offcanvas.Header closeButton>
-                                <Offcanvas.Title>Sobre mim e o site!</Offcanvas.Title>
-                                </Offcanvas.Header>
-                                <Offcanvas.Body>
-                                Some text as placeholder. In real life you can have the elements you
-                                have chosen. Like, text, images, lists, etc.
-                                </Offcanvas.Body>
-                            </Navbar.Offcanvas>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Bem-vindo</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Olá!</p>
+                        <p>Meu nome é Otavio, tenho 25 anos.</p>
+                        <p>Venho por meio deste site divulgar um pouco do meu trabalho e dos meus conhecimentos em algumas áreas de TI.</p>
+                        <p>Sou desenvolvedor FullStack em Nodejs e espero que você goste do conteúdo aqui apresentado, e caso tenha uma ideia de projeto ou sugestões para melhorias, ambas são bem-vindas.</p>
+                        <p>Obrigado!</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </header>
         )
     }
@@ -52,22 +63,29 @@ function Header(){
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="me-auto">
                                 <Button variant="dark" href="/">Home</Button>
-                            </Nav>
-                            <Nav>
                                 <Button variant="dark" onClick={handleShow}> Sobre </Button>
                             </Nav>
-                            <Navbar.Offcanvas show={show} onHide={handleClose}>
-                                <Offcanvas.Header closeButton>
-                                <Offcanvas.Title>Sobre mim e o site!</Offcanvas.Title>
-                                </Offcanvas.Header>
-                                <Offcanvas.Body>
-                                Some text as placeholder. In real life you can have the elements you
-                                have chosen. Like, text, images, lists, etc.
-                                </Offcanvas.Body>
-                            </Navbar.Offcanvas>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Bem-vindo</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Olá!</p>
+                        <p>Meu nome é Otavio, tenho 25 anos.</p>
+                        <p>Venho por meio deste site divulgar um pouco do meu trabalho e dos meus conhecimentos em algumas áreas de TI.</p>
+                        <p>Sou desenvolvedor FullStack em Nodejs e espero que você goste do conteúdo aqui apresentado, e caso tenha uma ideia de projeto ou sugestões para melhorias, ambas são bem-vindas.</p>
+                        <p>Obrigado!</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </header>
         )
     }
--- src/frontend/src/components/pages/Home.js
+++ src/frontend/src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import './styles.css'
-import { Card, CardGroup, Container, Button, ListGroup, Form } from 'react-bootstrap';
+import { Card, CardGroup, Container, Button, ListGroup, Form, Carousel } from 'react-bootstrap';
 import { FcDownRight, FcApproval } from "react-icons/fc";
 import emailjs from '@emailjs/browser'
-import ImageHome1 from '../img/programacao.jpg'
-import ImageHome2 from '../img/DevOps.png'
-import ImageHome3 from '../img/banco-de-dados.jpg'
+import ImageCard1 from '../img/programacao.jpg'
+import ImageCard2 from '../img/DevOps.png'
+import ImageCard3 from '../img/banco-de-dados.jpg'
+import ImageHome from '../img/home.png'
+import ImageHome2 from '../img/home2.png'
+import ImageHome3 from '../img/home3.png'
 
 function Home(){
     const [name, setName] = useState('')
@@ -42,102 +45,128 @@ function Home(){
     }
 
     return(
-        <Container id='div-principal-scroll'>
-            <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
-                <CardGroup id="home">
-                    <Card>
-                        <Card.Img variant="top" src={ImageHome1} width="100" height="250"/>
-                        <Card.Body>
-                            <Card.Title>Full Stack</Card.Title>
-                            <Card.Text>
-                            Em mundo globalizado onde as informações correm de maneira fácil e rápida, se especializar em uma única tecnologia ou ferramenta de trabalho, ou não conseguir se adaptar ao uso de novas tecnologias é um grande problema nos dias hoje, principalmente pra quem quer adentrar no mercado de TI.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Button variant="primary" href='/fullstack'>Veja mais sobre</Button>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src={ImageHome2} width="100" height="250"/>
-                        <Card.Body>
-                            <Card.Title>DevOps</Card.Title>
-                            <Card.Text>
-                            Não basta só desenvolver aplicações! É necessário desenvolver e operar as aplicações. Integrando e monitorando suas atividades para buscar um desempenho mais otimizado e simplificado.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Button variant="primary" href='/devops'>Veja mais sobre</Button>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src={ImageHome3} width="100" height="250"/>
-                        <Card.Body>
-                            <Card.Title>Banco de dados</Card.Title>
-                            <Card.Text>
-                            Além de armazenar dados. Como organizar os dados? Porque organizar os dados? Que tipo de banco de dados é necessário pra determinado tipo de aplicação? Alguns dos questionamentos que se deve fazer a um profissional de TI.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Button variant="primary" href='/bancodedados'>Veja mais sobre</Button>
-                        </Card.Footer>
-                    </Card>
-                </CardGroup>
+        <div>
+            <Carousel id="home">
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={ImageHome}
+                    alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={ImageHome2}
+                    alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={ImageHome3}
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
 
-                <div class="mb-4">
-                    <hr class="solid"/>
-                </div>
+            <Container id='div-principal-scroll'>
+                <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+                    <CardGroup id="conteudo">
+                        <Card>
+                            <Card.Img variant="top" src={ImageCard1} width="100" height="250"/>
+                            <Card.Body>
+                                <Card.Title>Full Stack</Card.Title>
+                                <Card.Text>
+                                Em mundo globalizado onde as informações correm de maneira fácil e rápida, se especializar em uma única tecnologia ou ferramenta de trabalho, ou não conseguir se adaptar ao uso de novas tecnologias é um grande problema nos dias hoje, principalmente pra quem quer adentrar no mercado de TI.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button variant="primary" href='/fullstack'>Veja mais sobre</Button>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src={ImageCard2} width="100" height="250"/>
+                            <Card.Body>
+                                <Card.Title>DevOps</Card.Title>
+                                <Card.Text>
+                                Não basta só desenvolver aplicações! É necessário desenvolver e operar as aplicações. Integrando e monitorando suas atividades para buscar um desempenho mais otimizado e simplificado.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button variant="primary" href='/devops'>Veja mais sobre</Button>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src={ImageCard3} width="100" height="250"/>
+                            <Card.Body>
+                                <Card.Title>Banco de dados</Card.Title>
+                                <Card.Text>
+                                Além de armazenar dados. Como organizar os dados? Porque organizar os dados? Que tipo de banco de dados é necessário pra determinado tipo de aplicação? Alguns dos questionamentos que se deve fazer a um profissional de TI.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button variant="primary" href='/bancodedados'>Veja mais sobre</Button>
+                            </Card.Footer>
+                        </Card>
+                    </CardGroup>
 
-                <div id='curriculo' className='div-h1-curriculo'>
-                    <h1 id='h1-curriculo'>Minha Formação</h1>
-                </div>
-
-                <ListGroup className='lista-curriculo'>
-                    <ListGroup.Item>
-                        <p><FcDownRight/> Formação</p>
-                        <p><FcApproval/> Bacharel em Ciência da Computação pela Universidade de Brasília, 2023</p>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <p><FcDownRight/> Experiência Profissional</p>
-                        <p><FcApproval/> Treinamento DevOps/Fullstack Node.js na DATAPREV - 1 ano de experiência como estagiário</p>
-                        <p><FcApproval/> DevWeb/analista de dados/Suporte na ANAC - 2 anos de experiência como estagiário</p>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <p><FcDownRight/> Conhecimentos</p>
-                        <p><FcApproval/> Conhecimentos em Javascript, Banco de dados relacionais (MySQL, Oracle, Postgres), MUI, Bootstrap, kubernetes, docker e ansible </p>
-                        <p><FcApproval/>Desenvolvedor Full Stack (Node.js + React + Javascript + Swagger) e especialista em desenvolvimento Web utilizando framework Oracle Apex</p>
-                    </ListGroup.Item>
-                </ListGroup>
-
-                <div class="mb-4">
-                    <hr class="solid"/>
-                </div>
-
-                <div className='div-h1-contato' id='contato'>
-                    <h1 id='h1-contato'>Entre em Contato</h1>
-                </div>
-
-                <Form className='formulario-contato' onSubmit={sendEmail}>
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Nome</Form.Label>
-                        <Form.Control type="text" placeholder="ex: João da Silva" value={name} onChange={(e) => setName(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>E-mail</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
-                        <Form.Label>Motivo do contato</Form.Label>
-                        <Form.Control as="textarea" rows={3} placeholder="Motivo do contato" value={message} onChange={(e) => setMessage(e.target.value)}/>
-                    </Form.Group>
-
-                    <div className="d-grid gap-2">
-                        <Button variant="primary" type="submit" size="lg">
-                            Enviar
-                        </Button>
+                    <div class="mb-4">
+                        <hr class="solid"/>
                     </div>
-                </Form>
-            </div>
-        </Container>
+
+                    <div id='curriculo' className='div-h1-curriculo'>
+                        <h1 id='h1-curriculo'>Minha Formação</h1>
+                    </div>
+
+                    <ListGroup className='lista-curriculo'>
+                        <ListGroup.Item>
+                            <p><FcDownRight/> Formação</p>
+                            <p><FcApproval/> Bacharel em Ciência da Computação pela Universidade de Brasília, 2023</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <p><FcDownRight/> Experiência Profissional</p>
+                            <p><FcApproval/> Treinamento DevOps/Fullstack Node.js na DATAPREV - 1 ano de experiência como estagiário</p>
+                            <p><FcApproval/> DevWeb/analista de dados/Suporte na ANAC - 2 anos de experiência como estagiário</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <p><FcDownRight/> Conhecimentos</p>
+                            <p><FcApproval/> Conhecimentos em Javascript, Banco de dados relacionais (MySQL, Oracle, Postgres), MUI, Bootstrap, kubernetes, docker e ansible </p>
+                            <p><FcApproval/>Desenvolvedor Full Stack (Node.js + React + Javascript + Swagger) e especialista em desenvolvimento Web utilizando framework Oracle Apex</p>
+                        </ListGroup.Item>
+                    </ListGroup>
+
+                    <div class="mb-4">
+                        <hr class="solid"/>
+                    </div>
+
+                    <div className='div-h1-contato' id='contato'>
+                        <h1 id='h1-contato'>Entre em Contato</h1>
+                    </div>
+
+                    <Form className='formulario-contato' onSubmit={sendEmail}>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control type="text" placeholder="ex: João da Silva" value={name} onChange={(e) => setName(e.target.value)}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>E-mail</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
+                            <Form.Label>Motivo do contato</Form.Label>
+                            <Form.Control as="textarea" rows={3} placeholder="Motivo do contato" value={message} onChange={(e) => setMessage(e.target.value)}/>
+                        </Form.Group>
+
+                        <div className="d-grid gap-2">
+                            <Button variant="primary" type="submit" size="lg">
+                                Enviar
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
+            </Container>
+        </div>
     )
 }
 
