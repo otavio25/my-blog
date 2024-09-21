bb5a6b5bdb1407217ba8f5a2f7a71d21f8d434ba
otavio25 <souzaotavio721@gmail.com>
2024-09-21 00:00:44 -0300

    refactor: Refaz a estrutura da home adicionando novos projetos

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,6 +8,8 @@ import ImageHome3 from '../img/home3.png'
 import ImageCard1 from '../img/logo_sistema_aeroportos.png'
 import ImageCard2 from '../img/faas.jpg'
 import ImageCard3 from '../img/programacao.jpg'
+import ImageCard4 from '../img/sistema_gerenciamento_equipamento.png'
+import ImageCard5 from '../img/scan_vuknerabilidade_wp.png'
 import Curriculo from '../img/curriculo.png'
 import Swal from 'sweetalert2'
 
@@ -78,51 +80,55 @@ function Home(){
             </Carousel>
 
             <Container>
-                <div class="mb-4">
-                    <hr class="solid"/>
-                </div>
-                <CardGroup id="conteudo">
-                    <Card>
-                        <Card.Img variant="top" src={ImageCard1} width="100" height="250"/>
-                        <Card.Body>
-                            <Card.Title>Sistema para Serviços de Aeroporto</Card.Title>
-                            <Card.Text>
-                            Projeto que permite todo o gerenciamento de pessoal incluindo cursos, documentos e certificados. Para acessar o sistema utilize username = teste e senha = teste1234 e brinque à vontade.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Button variant="primary" href='https://apex.oracle.com/pls/apex/r/network_app/msc-controle-de-treinamento/login?session=17545323933662' target="_blank" rel="noopener noreferrer">Veja</Button>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src={ImageCard2} width="100" height="250"/>
-                        <Card.Body>
-                            <Card.Title>Trabalho de Conclusão de Curso</Card.Title>
-                            <Card.Text>
-                            Monografia apresentada como requisito parcial para conclusão do Bacharelado em Ciência da Computação na Universidade de Brasília.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Button variant="primary" href='https://drive.google.com/file/d/1jneZc-Vy_SOZX8TlUlgYqXiaIyd7eqnn/view?usp=sharing' target="_blank" rel="noopener noreferrer">Veja</Button>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src={ImageCard3} width="100" height="250"/>
-                        <Card.Body>
-                            <Card.Title>QSA Info</Card.Title>
-                            <Card.Text>
-                            Projeto voltado aos dados do QSA, retorna uma listagem de empresas e seus sócios e mostra um gráfico desse relacionamento.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <Button variant="primary" href='https://github.com/otavio25/qsa-info' target="_blank" rel="noopener noreferrer">Veja</Button>
-                        </Card.Footer>
-                    </Card>
-                </CardGroup>
+                <hr class="featurette-divider"/>
 
-                <div class="mb-4">
-                    <hr class="solid"/>
+                <div class="container marketing" id="conteudo">
+                    <div class="row text-center">
+                        <div class="col-lg-4">
+                            <img class="bd-placeholder-img rounded-circle" width="140" height="140" src={ImageCard1} role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+
+                            <h2>Sistema para Serviços de Aeroporto</h2>
+                            <p>Projeto que permite todo o gerenciamento de pessoal incluindo cursos, documentos e certificados. Para acessar o sistema utilize username = teste e senha = teste1234 e brinque à vontade.</p>
+                            <p><a class="btn btn-secondary" href='https://apex.oracle.com/pls/apex/r/network_app/msc-controle-de-treinamento/login?session=17545323933662' target="_blank" rel="noopener noreferrer">Ver detalhes »</a></p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img class="bd-placeholder-img rounded-circle" width="140" height="140" src={ImageCard2} role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+                            <h2>Trabalho de Conclusão de Curso</h2>
+                            <p>Monografia apresentada como requisito parcial para conclusão do Bacharelado em Ciência da Computação na Universidade de Brasília.</p>
+                            <p><a class="btn btn-secondary" href='https://drive.google.com/file/d/1jneZc-Vy_SOZX8TlUlgYqXiaIyd7eqnn/view?usp=sharing' target="_blank" rel="noopener noreferrer">Ver detalhes »</a></p>
+                        </div>
+                        <div class="col-lg-4">
+                            <img class="bd-placeholder-img rounded-circle" width="140" height="140" src={ImageCard3} role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+                            <h2>QSA Info</h2>
+                            <p>Projeto voltado aos dados do QSA, retorna uma listagem de empresas e seus sócios e mostra um gráfico desse relacionamento.</p>
+                            <p><a class="btn btn-secondary" href='https://github.com/otavio25/qsa-info' target="_blank" rel="noopener noreferrer">Ver detalhes »</a></p>
+                        </div>
+                    </div>
+                    <hr class="featurette-divider"/>
+                    <div class="row featurette">
+                        <div class="col-md-7">
+                            <h2 class="featurette-heading">Scan Vulnerabilidade WP </h2>
+                            <p class="lead">Este sistema tem o objetivo de retornar dados do banco de dados CVE (Common Vulnerabilities and Exposures), especificamente relacionados a sites desenvolvidos com WordPress. Isso inclui a exibição de quaisquer vulnerabilidades associadas a temas, plugins e versões do WordPress encontrados no site alvo.</p>
+                            <p><a class="btn btn-secondary" href='https://github.com/otavio25/frontend-scan-vulnerabilidade-wp' target="_blank" rel="noopener noreferrer">Ver detalhes »</a></p>
+                        </div>
+                        <div class="col-md-5">
+                            <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src={ImageCard5} role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+                        </div>
+                    </div>
+                    <hr class="featurette-divider"/>
+                    <div class="row featurette">
+                        <div class="col-md-7 order-md-2">
+                            <h2 class="featurette-heading">Sistema de Gerenciamento de Equipamentos</h2>
+                            <p class="lead">Sistema de gerenciamento de equipamentos de aeroportos. O sistema permite o cadastro de vários tipos de equipamento e seus gerenciamento desde a abertura de Ordens de Serviço quanto ao seu custo. Para acessar o sistema utilize username = teste e senha = teste1234 e brinque à vontade.</p>
+                            <p><a class="btn btn-secondary" href='https://apex.oracle.com/pls/apex/r/network_app/sm/login?session=5858922611615' target="_blank" rel="noopener noreferrer">Ver detalhes »</a></p>
+                        </div>
+                        <div class="col-md-5 order-md-1">
+                            <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src={ImageCard4} role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+                        </div>
+                    </div>
                 </div>
+
+                <hr class="featurette-divider"/>
 
                 <div id='curriculo' className='div-h1-curriculo'>
                     <h1 id='h1-curriculo'>Minha Formação</h1>
@@ -137,14 +143,12 @@ function Home(){
                     />
                 </Figure>
 
-                <div class="mb-4">
-                    <hr class="solid"/>
-                </div>
+                <hr class="featurette-divider"/>
 
                 <div className='div-h1-contato' id='contato'>
                     <h1 id='h1-contato'>Entre em Contato</h1>
                 </div>
-
+            
                 <Form className='formulario-contato' onSubmit={sendEmail}>
                     <Form.Group className="mb-3" >
                         <Form.Label>Nome</Form.Label>
